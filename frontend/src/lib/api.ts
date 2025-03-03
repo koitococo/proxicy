8be020887b0f19110b9222b8f89ce8d173f9d6d7
hawa130 <handanwanga@hotmail.com>
2025-03-03 21:14:58 +0800

    chore: dependencies

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,5 +1,4 @@
 import { treaty } from '@elysiajs/eden'
 import type { App } from 'proxicy-ts'
 
-// @ts-expect-error App type is not satisfied
 export const api = treaty<App>(import.meta.env.VITE_BASE_URL)
