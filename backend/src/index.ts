3ebffd2b8334ba7d340c96bd083148fa431bb217
hawa130 <handanwanga@hotmail.com>
2025-03-03 19:56:14 +0800

    feat(backend): add cors plugin and update swagger config

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,15 +1,44 @@
 import { Elysia } from "elysia";
+import { cors } from "@elysiajs/cors";
 import { swagger } from "@elysiajs/swagger";
 import { opentelemetry } from "@elysiajs/opentelemetry";
 import { routes } from "@/api";
 import { loggerPlugin } from "@/plugins/loggerPlugin";
-import { PORT } from "@/utils/config";
+import { ALLOWED_ORIGINS, PORT } from "@/utils/config";
 
 const app = new Elysia()
   .use(loggerPlugin)
-  .use(swagger())
+  .use(
+    // @ts-expect-error
+    cors({
+      origin: ALLOWED_ORIGINS,
+    }),
+  )
+  .use(
+    // @ts-expect-error
+    swagger({
+      documentation: {
+        components: {
+          securitySchemes: {
+            bearerAuth: {
+              type: "http",
+              scheme: "bearer",
+            },
+          },
+        },
+        info: {
+          title: "Proxicy API Documentation",
+          version: "0.0.1",
+        },
+      },
+    }),
+  )
   .use(opentelemetry())
   .use(routes)
   .listen(PORT);
+
+console.log(
+  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`,
+);
 
 export type App = typeof app;
